5e0601d7c538dbb0cd8ebd003b6b49fd7b4d0f34
Thanapapas Horsuwan <thanapapas@hotmail.com>
2019-02-22 04:14:22 +0700

    create output detector if not exist

--- ChulaSSSEnv.py
+++ ChulaSSSEnv.py
@@ -108,7 +108,9 @@ class ChulaSSSEnv(gym.Env):
         self.view_file = '{}/gui-settings/gui-settings-file-loop.xml'.format(self.root_dir) if (env_config['viewport'] == "whole_loop") \
                          else '{}/gui-settings/gui-settings-file-surasak.xml'.format(self.root_dir)
         self.detector_file = '{}/detectors/sathorn_w_detectors.add.xml'.format(self.root_dir)
+        print("I am checking if there is path ")
         if not os.path.exists(os.path.join(os.path.split(self.detector_file)[0])):
+            print("There is no path {}, Building ...".format(os.path.join(os.path.split(self.detector_file)[0])))
             os.makedirs(os.path.join(os.path.split(self.detector_file)[0]))
         self.begin_time = 21600 if (env_config['time_select'] == "morning") else 53100
         self.end_time   = 32400 if (env_config['time_select'] == "morning") else 69300
