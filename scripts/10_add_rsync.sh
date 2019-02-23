373566422491e4849d86180a5f019e397ebd5ee5
Thanapapas Horsuwan <thanapapas@hotmail.com>
2019-02-24 05:00:39 +0700

    added rsync script

---
+++ scripts/10_add_rsync.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+gsutil -m cp -r ~/ray_results/$(ls -t ~/ray_results | head -1) gs://ray_results
+
+while true
+do
+    gsutil -m rsync -r ~/ray_results/$(ls -t ~/ray_results | head -1) gs://ray_results
+    sleep 600
+done
